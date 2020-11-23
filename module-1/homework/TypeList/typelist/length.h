4358410826ad41ad3b6c6de9ae3be4fe800ca78a
evgeniy <kletska.epa@hotmail.com>
2020-11-23 22:47:22 +0300

    Уже можно писать в резюме, что я метапрограммист?

--- module-1/homework/TypeList/typelist/length.h
+++ module-1/homework/TypeList/typelist/length.h
@@ -2,5 +2,12 @@
 
 #include "typelist.h"
 
-template<typename TList> 
-struct Length;
+template <typename TList>
+struct Length {
+  static constexpr int length = Length<typename TList::T>::length + 1;
+};
+
+template <>
+struct Length<NullType> {
+  static constexpr int length = 0;
+};
--- module-1/homework/TypeList/typelist/typeat.h
+++ module-1/homework/TypeList/typelist/typeat.h
@@ -2,5 +2,17 @@
 
 #include "typelist.h"
 
-template<typename TList, unsigned int index>
-struct TypeAt;
+template <typename TList, unsigned int index>
+struct TypeAt {
+  typedef typename TypeAt<typename TList::T, index - 1>::TargetType TargetType;
+};
+
+template <typename TList>
+struct TypeAt<TList, 0> {
+  typedef typename TList::H TargetType;
+};
+
+template <unsigned int index>
+struct TypeAt<NullType, index> {
+  typedef NullType TargetType;
+};
